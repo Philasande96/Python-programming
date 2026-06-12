6be1fcb0d01ebe501efc9351010fb4c4113a335e
Philasande Mathafeni <labale1996@gmail.com>
2026-06-12 20:59:37 +0200

    ///

---
+++ turples.py
@@ -0,0 +1,4 @@
+#Turples are immutable, ordered collections of items. They are similar to lists, but they cannot be modified after they are created. Tuples are defined using parentheses () and can contain any type of data, including other tuples. Here is an example of how to create and use tuples in Python:
+
+real_numbers = (1, 2, 3, 4, 5) #create a tuple of real numbers
+print(real_numbers) #print the tuple of real numbers
