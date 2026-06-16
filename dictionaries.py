22596b91ae98854639bbb8fd5b9e043a763885a3
Philasande Mathafeni <labale1996@gmail.com>
2026-06-16 02:30:41 +0200

    Intro to dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,6 @@
+#learning about dictionaries in Python 
+#What is a dictionary? A Python dictionary is a collection of items, similar to lists and tuples. However, unlike lists and tuples, each item in a dictionary is a key-value pair (consisting of a key and a value).
+#We use curly braces {} to declare the dictionary
+
+my_son = {'Name': 'Liqhame', 'Surname': 'Mathafeni', 'age': 3, 'Gender': 'Male', 'Location': 'Dutywa'} #Declaring the dictionary 
+print(f'hellow {my_son['Name']} {my_son['Surname']} we have discovered that you are {my_son["age"]}yrs,{my_son['Gender']} in gender and you are living in {my_son['Location']}')
